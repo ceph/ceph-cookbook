0da830a6e86ae659e230469417d6104c7fb4c7e2
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-09-27 16:00:36 -0700

    Add chef::single_mon recipe for simple 1-node monitor clusters.

---
+++ ceph/recipes/single_mon.rb
@@ -0,0 +1,77 @@
+# this recipe creates a single-node monitor cluster
+
+include_recipe "ceph::mon"
+
+template '/etc/ceph/ceph.conf' do
+  source 'ceph.conf.single_mon.erb'
+  variables(
+    :mon_address => "#{node[:ipaddress]}:6789"
+  )
+  mode '0644'
+end
+
+execute 'create client.admin keyring' do
+  creates '/etc/ceph/client.admin.keyring'
+  command <<-EOH
+set -e
+cauthtool \
+  --create-keyring \
+  --gen-key \
+  --name=client.admin \
+  --set-uid=0 \
+  --cap mon 'allow *' \
+  --cap osd 'allow *' \
+  --cap mds 'allow' \
+  /etc/ceph/client.admin.keyring.tmp
+mv /etc/ceph/client.admin.keyring.tmp /etc/ceph/client.admin.keyring
+EOH
+end
+
+execute 'ceph-mon mkfs' do
+  # TODO this is probably not an atomic test
+  creates '/srv/mon.single/magic'
+  command <<-EOH
+set -e
+install -d -m0700 /srv/mon.single.temp
+cauthtool --create-keyring --gen-key --name=mon. /srv/mon.single.temp/keyring
+cat /etc/ceph/client.admin.keyring >>/srv/mon.single.temp/keyring
+monmaptool --create --clobber --add single #{node[:ipaddress]} /srv/mon.single.temp/monmap
+osdmaptool --clobber --createsimple 1 /srv/mon.single.temp/osdmap
+cmon --mkfs -i single --monmap=/srv/mon.single.temp/monmap --osdmap=/srv/mon.single.temp/osdmap --keyring=/srv/mon.single.temp/keyring
+rm -rf /srv/mon.single.temp
+EOH
+end
+
+service 'ceph' do
+  action [:enable, :start]
+end
+
+
+# this keyring will be used by filestore nodes to add new osd
+# instances
+execute 'create client.bootstrap-osd keyring' do
+  creates '/etc/ceph/client.bootstrap-osd.keyring'
+  command <<-EOH
+set -e
+cauthtool \
+  --create-keyring \
+  --gen-key \
+  --name=client.bootstrap-osd \
+  /etc/ceph/client.bootstrap-osd.keyring.tmp
+mv /etc/ceph/client.bootstrap-osd.keyring.tmp /etc/ceph/client.bootstrap-osd.keyring
+EOH
+end
+
+execute 'authorize client.bootstrap-osd' do
+  command <<-EOH
+set -e
+ceph auth add \
+  -i /etc/ceph/client.bootstrap-osd.keyring \
+  client.bootstrap-osd \
+  mon \
+    "allow command osd create; \
+    allow command osd crush add ...; \
+    allow command auth add * osd allow\\ * mon allow\\ rwx; \
+    allow command mon getmap"
+EOH
+end
