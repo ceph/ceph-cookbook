d084548bdb71548cf1165961b09a7d632d3c91fc
Kyle Bader <kyle.bader@dreamhost.com>
2011-09-27 20:09:23 -0700

    Readme was a stub and not really required for something so simple

--- btrfs/metadata.rb
+++ btrfs/metadata.rb
@@ -2,5 +2,4 @@ maintainer			"Kyle Bader"
 maintainer_email	"kyle.bader@dreamhost.com"
 license				"Apache 2.0"
 description			"Installs tools for BTRFS filesystems"
-long_description	IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version				"0.0.1"
