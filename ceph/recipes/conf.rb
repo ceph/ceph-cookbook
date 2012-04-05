dc5b95e0631ba8503d4ece108febc29d29148fbd
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2012-04-05 15:08:30 -0700

    Use roles not recipes for searching.
    The list of recipes a node has is only updated after
    a successful run, and in this case the first run using
    single_mon fails because ceph.conf gets no mon addresses.
    
    Use a role called "ceph-mon" instead. That should have,
    in its runlist, ceph::run_mon and ceph::single_mon.

--- ceph/recipes/conf.rb
+++ ceph/recipes/conf.rb
@@ -1,7 +1,7 @@
 if is_crowbar?
-  mon_addresses = search(:node, "recipes:ceph\\:\\:single_mon AND ceph_config_environment:#{node[:ceph][:config][:environment]}").map { |node| Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, "admin").address + ":6789" }
+  mon_addresses = search(:node, "role:ceph-mon AND ceph_config_environment:#{node[:ceph][:config][:environment]}").map { |node| Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, "admin").address + ":6789" }
 else
-  mon_addresses = search(:node, "recipes:ceph\\:\\:single_mon AND chef_environment:#{node.chef_environment}").map { |node| node["ipaddress"] + ":6789" }
+  mon_addresses = search(:node, "role:ceph-mon AND chef_environment:#{node.chef_environment}").map { |node| node["ipaddress"] + ":6789" }
 end
 
 template '/etc/ceph/ceph.conf' do
