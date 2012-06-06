747601a9173b87197dc92947ce11518270ea53c7
Tommi Virtanen <tv@inktank.com>
2012-06-05 17:11:42 -0700

    Set ceph.conf fsid and mon_initial_members based on Chef attributes.
    Setting an environment to contain e.g.
    
      "default_attributes": {
        "ceph": {
          "config": {
            "fsid": "85749980-4bc2-47ce-8f6f-9064df84fe06",
            "mon_initial_members": ["foo"]
          }
        }
      },
    
    will set the variables in ceph.conf, ensuring multiple monitors
    coming up concurrently construct a consistent cluster, and only
    one of them.
    
    Contents of mon_initial_members need to be chosen with care. Please
    refer to the documentation (once it's ready).

--- ceph/recipes/conf.rb
+++ ceph/recipes/conf.rb
@@ -7,6 +7,8 @@ end
 template '/etc/ceph/ceph.conf' do
   source 'ceph.conf.erb'
   variables(
+    :fsid => node["ceph"]["config"]["fsid"],
+    :mon_initial_members => node["ceph"]["config"]["mon_initial_members"],
     :mon_addresses => mon_addresses
   )
   mode '0644'
