00bf19321dccab2a38e6065ac456e8575e6ebb7e
Tommi Virtanen <tv@inktank.com>
2012-06-08 15:40:22 -0700

    Passthrough arbitrary (global) config variables to ceph.conf.
    Still sanity checking that the ones we want are provided.

--- ceph/recipes/conf.rb
+++ ceph/recipes/conf.rb
@@ -1,10 +1,11 @@
+raise "fsid must be set in config" if node["ceph"]["config"]['fsid'].nil?
+raise "mon_initial_members must be set in config" if node["ceph"]["config"]['mon_initial_members'].nil?
+
 mon_addresses = get_mon_addresses()
 
 template '/etc/ceph/ceph.conf' do
   source 'ceph.conf.erb'
   variables(
-    :fsid => node["ceph"]["config"]["fsid"],
-    :mon_initial_members => node["ceph"]["config"]["mon_initial_members"],
     :mon_addresses => mon_addresses
   )
   mode '0644'
