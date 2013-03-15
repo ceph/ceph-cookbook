e97fc7bacaca5417c68bb5728d148b07d792d72b
Alexandre Marangone <alexandre.marangone@inktank.com>
2013-03-15 12:13:01 -0700

    Update apt configuration and recipes
    The configuration changes aims to be more in-line with ceph-deploy.
    Changing key and repository for gitbuilder.
    
    Signed-off-by: Alexandre Marangone <alexandre.marangone@inktank.com>

--- recipes/apt.rb
+++ recipes/apt.rb
@@ -1,13 +1,13 @@
 include_recipe "apt"
 
 case node['ceph']['branch']
-when "release"
-  apt_repository "ceph-release" do
+when "stable"
+  apt_repository "ceph-stable" do
     repo_name "ceph"
     uri "http://www.ceph.com/debian-#{node['ceph']['version']}/"
     distribution node['lsb']['codename']
     components ["main"]
-    key "https://raw.github.com/ceph/ceph/master/keys/release.asc"
+    key "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc"
   end
 when "testing"
   apt_repository "ceph-testing" do
@@ -15,14 +15,14 @@ when "testing"
     uri "http://www.ceph.com/debian-testing/"
     distribution node['lsb']['codename']
     components ["main"]
-    key "https://raw.github.com/ceph/ceph/master/keys/release.asc"
+    key "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc"
   end
-when "autobuild"
-  apt_repository "ceph-autobuild" do
+when "dev"
+  apt_repository "ceph-gitbuilder" do
     repo_name "ceph"
-    uri "http://gitbuilder.ceph.com/ceph-deb-#{node['lsb']['codename']}-x86_64-basic/ref/autobuild"
+    uri "http://gitbuilder.ceph.com/ceph-deb-#{node['lsb']['codename']}-x86_64-basic/ref/#{node['ceph']['version']}"
     distribution node['lsb']['codename']
     components ["main"]
-    key "https://raw.github.com/ceph/ceph/master/keys/autobuild.asc"
+    key "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc"
   end
 end
