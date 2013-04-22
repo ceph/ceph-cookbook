96a299831da302ac21eba522b18cfa3bc56d06a5
Alexandre Marangone <alexandre.marangone@inktank.com>
2013-04-22 16:41:05 -0700

    Support apache and mod_fastcgi forks for RGW
    Signed-off-by: Alexandre Marangone <alexandre.marangone@inktank.com>

--- attributes/radosgw.rb
+++ attributes/radosgw.rb
@@ -21,3 +21,4 @@ default["ceph"]["radosgw"]["admin_email"] = "admin@example.com"
 default["ceph"]["radosgw"]["rgw_addr"] = "*:80"
 default["ceph"]["radosgw"]["rgw_port"] = false
 default["ceph"]["radosgw"]["webserver_companion"] = "apache2" #can be false
+default['ceph']["radosgw"]['use_apache_fork'] = true
