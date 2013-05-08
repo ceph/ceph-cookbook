20ab85e02596c832917536f59827ed9df92e0f70
Alexandre Marangone <alexandre.marangone@inktank.com>
2013-05-08 15:06:16 -0700

    Repo: support custom rpm repos
    Signed-off-by: Alexandre Marangone <alexandre.marangone@inktank.com>

--- recipes/rpm.rb
+++ recipes/rpm.rb
@@ -1,47 +1,36 @@
-case node['platform_family']
+platform_family = node['platform_family']
+
+case platform_family
 when "rhel"
-  version =  %x[ cat /etc/redhat-release | awk '{print $3}' | awk -F. '{print $1}' ].chomp
-  release = "el" + version
   if node['ceph']['el_add_epel'] == true
     # We need to do this since the EPEL
     # version might change
+    version = node['platform_version'].to_i
     epel_package = %x[ curl -s http://dl.fedoraproject.org/pub/epel/fullfilelist | grep ^#{version}/#{node['kernel']['machine']}/epel-release ].chomp
     system "rpm -U http://dl.fedoraproject.org/pub/epel/#{epel_package}"
   end
-when "fedora"
-  version = %x[ cat /etc/fedora-release | awk '{print $3}' ].chomp
-  release = "fc" + version
-when "suse"
-  suse = %x[ head -n1 /etc/SuSE-release| awk '{print $1}' ].chomp.downcase #can be suse or opensuse
-  version = %x[ grep VERSION /etc/SuSE-release | awk -F'= ' '{print $2}' ].chomp
-  release = suse + version
 end
 
-end_path = "/#{release}/x86_64/ceph-release-1-0.#{release}.noarch.rpm"
-case node['ceph']['branch']
-when "stable"
-  path = "http://ceph.com/rpm-#{node['ceph']['version']}" + end_path
-  system "rpm -U #{path}"
-when "testing"
-  path = "http://ceph.com/rpm-testing" + end_path
-  system "rpm -U #{path}"
-when "dev"
-  if node['platform'] == "centos"
-    baseurl="http://gitbuilder.ceph.com/ceph-rpm-centos#{version}-x86_64-basic/ref/#{node['ceph']['version']}/x86_64/"
-  elsif node['platform'] == "fedora"
-    baseurl="http://gitbuilder.ceph.com/ceph-rpm-#{release}-x86_64-basic/ref/#{node['ceph']['version']}/RPMS/x86_64/"
-  else
-    raise "repository not available for your distribution"
-  end
+branch = node['ceph']['branch']
+if branch == "dev" and platform_family != "centos" and platform_family != "fedora"
+  raise "Dev branch for #{platform_family} is not yet supported"
+end
+
+repo = node['ceph'][platform_family][branch]['repository']
+
+if branch == "dev"
   # Instead of using the yum cookbook,
   # we do it this way. It avoids a dependency
-  system "curl -s 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc' > /etc/pki/rpm-gpg/RPM-GPG-KEY-CEPH"
+  system "curl -s node['ceph'][#{platform_family}]['dev']['repository_key'] > /etc/pki/rpm-gpg/RPM-GPG-KEY-CEPH"
   system "cat > /etc/yum.repos.d/ceph.repo << EOF\n" \
     "[ceph]\n" \
     "name=Ceph\n" \
-    "baseurl=#{baseurl}\n" \
+    "baseurl=#{repo}\n" \
     "enabled=1\n" \
     "gpgcheck=1\n" \
     "gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CEPH\n" \
     "EOF\n"
+else
+  #This is a stable or testing branch
+  system "rpm -U #{node['ceph'][platform_family][branch]['repository']}"
 end
