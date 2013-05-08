20cd0d62a58b0326f34f4910474bc267083a0d49
Alexandre Marangone <alexandre.marangone@inktank.com>
2013-05-08 09:55:12 -0700

    Update stable release to Cuttlefish
    Signed-off-by: Alexandre Marangone <alexandre.marangone@inktank.com>

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -1,4 +1,4 @@
 default['ceph']['branch'] = "stable" # Can be stable, testing or dev.
 # Major release version to install or gitbuilder branch
-default['ceph']['version'] = "bobtail"
+default['ceph']['version'] = "cuttlefish"
 default['ceph']['el_add_epel'] = true
