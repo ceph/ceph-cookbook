c49da7eab8efc3ad57eeb1630243d5c5e1399d30
Alexandre Marangone <alexandre.marangone@inktank.com>
2013-05-08 11:32:02 -0700

    repo: support custom apt repos
    Signed-off-by: Alexandre Marangone <alexandre.marangone@inktank.com>

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -2,3 +2,9 @@ default['ceph']['branch'] = "stable" # Can be stable, testing or dev.
 # Major release version to install or gitbuilder branch
 default['ceph']['version'] = "cuttlefish"
 default['ceph']['el_add_epel'] = true
+default['ceph']['debian']['stable']['repository'] = "http://ceph.com/debian-#{node['ceph']['version']}/"
+default['ceph']['debian']['stable']['repository_key'] = "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc"
+default['ceph']['debian']['testing']['repository'] = "http://www.ceph.com/debian-testing/"
+default['ceph']['debian']['testing']['repository_key'] = "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc"
+default['ceph']['debian']['dev']['repository'] = "http://gitbuilder.ceph.com/ceph-deb-#{node['lsb']['codename']}-x86_64-basic/ref/#{node['ceph']['version']}"
+default['ceph']['debian']['dev']['repository_key'] = "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc"
