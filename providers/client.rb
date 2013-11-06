789855e996cbe682c904a3c3a10968ed6dce3b11
Walter  Huf <walter.huf@corvisa.com>
2013-11-06 10:33:51 -0600

    Notifies when the keyring is changed
    Also handles updating when the capabilities change

--- providers/client.rb
+++ providers/client.rb
@@ -1,15 +1,40 @@
+require "open3"
+
+def get_key(keyname, caps, secret)
+  ret = {}
+  Open3.popen3("ceph auth get-or-create #{keyname} #{caps} --name mon. --key='#{secret}'") {|stdin, stdout, stderr, wait|
+    ret['stdout'] = stdout.read
+    ret['stderr'] = stderr.read
+  }
+  return ret
+end
+
 action :add do
-  name = new_resource.name
-  desc = new_resource.description || new_resource.name
-  keyname = new_resource.keyname || "client.#{name}.#{node['hostname']}"
-  filename = new_resource.filename || "/etc/ceph/ceph.client.#{name}.#{node['hostname']}.keyring"
-  caps = new_resource.caps.select{|k,v|true}.map{|(k,v)| "#{k} '#{v}'"}.join(' ')
-  ruby_block "create #{desc} client key" do
-    block do
-      keyring = %x[ ceph auth get-or-create #{keyname} #{caps} --name mon. --key='#{node["ceph"]["monitor-secret"]}' ]
-      keyfile = ::File.new(filename, "w")
-      keyfile.puts(keyring)
-      keyfile.close
-    end
+  name = @new_resource.name
+  desc = @new_resource.description || @new_resource.name
+  keyname = @new_resource.keyname || "client.#{name}.#{node['hostname']}"
+  filename = @new_resource.filename || "/etc/ceph/ceph.client.#{name}.#{node['hostname']}.keyring"
+  caps = @new_resource.caps.select{|k,v|true}.map{|(k,v)| "#{k} '#{v}'"}.join(' ')
+
+  orig = ::IO.read(filename) rescue ""
+
+  # register the key
+  status = get_key(keyname, caps, node["ceph"]["monitor-secret"])
+  if status['stderr'].scan(/EINVAL.*but cap.*does not match/)
+    # delete an old key if it exists and is wrong
+    `ceph auth del #{keyname}`
+    status = get_key(keyname, caps, node["ceph"]["monitor-secret"])
+  end
+  if status['stderr'] != ''
+    raise "ceph auth: #{status['stderr']}"
+  end
+
+  # save the key
+  keyring = status['stdout']
+  if orig != keyring
+    keyfile = ::File.new(filename, "w")
+    keyfile.puts(keyring)
+    keyfile.close
+    @new_resource.updated_by_last_action(true)
   end
 end
