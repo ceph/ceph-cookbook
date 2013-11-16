0aa062ce20b5e3be8fd501029fdd475cae47682c
Guilhem Lettron <guilhem@lettron.fr>
2013-11-16 15:18:53 +0100

    Fix provider with best practices * use load_current_resource * use Mixlib:ShellOut * ...

--- providers/client.rb
+++ providers/client.rb
@@ -1,40 +1,49 @@
-require "open3"
-
-def get_key(keyname, caps, secret)
-  ret = {}
-  Open3.popen3("ceph auth get-or-create #{keyname} #{caps} --name mon. --key='#{secret}'") {|stdin, stdout, stderr, wait|
-    ret['stdout'] = stdout.read
-    ret['stderr'] = stderr.read
-  }
-  return ret
-end
 
 action :add do
-  name = @new_resource.name
-  desc = @new_resource.description || @new_resource.name
-  keyname = @new_resource.keyname || "client.#{name}.#{node['hostname']}"
-  filename = @new_resource.filename || "/etc/ceph/ceph.client.#{name}.#{node['hostname']}.keyring"
-  caps = @new_resource.caps.select{|k,v|true}.map{|(k,v)| "#{k} '#{v}'"}.join(' ')
+  auth_set_key(current_resource.keyname, current_resource.caps) unless current_resource.exists
 
-  orig = ::IO.read(filename) rescue ""
-
-  # register the key
-  status = get_key(keyname, caps, node["ceph"]["monitor-secret"])
-  if status['stderr'].scan(/EINVAL.*but cap.*does not match/)
-    # delete an old key if it exists and is wrong
-    `ceph auth del #{keyname}`
-    status = get_key(keyname, caps, node["ceph"]["monitor-secret"])
+  file filename do
+    content lazy {auth_get_key(keyname)}
+    owner "root"
+    group "root"
+    mode "640"
   end
-  if status['stderr'] != ''
-    raise "ceph auth: #{status['stderr']}"
-  end
+end
 
-  # save the key
-  keyring = status['stdout']
-  if orig != keyring
-    keyfile = ::File.new(filename, "w")
-    keyfile.puts(keyring)
-    keyfile.close
-    @new_resource.updated_by_last_action(true)
+def load_current_resource
+  @current_resource = Chef::Resource::CephClient.new(@new_resource.name)
+  @current_resource.name(@new_resource.name)
+  @current_resource.keyname(@new_resource.keyname || "client.#{current_resource.name}.#{node['hostname']}")
+  @current_resource.filename(@new_resource.filename || "/etc/ceph/ceph.client.#{current_resource.name}.#{node['hostname']}.keyring")
+  if String === @new_resource.caps
+    @current_resource.caps(@new_resource.caps)
+  else
+    @current_resource.caps(@new_resource.caps.map{|k,v| "#{k} '#{v}'"}.join(' '))
+  end
+  @current_resource.exists = true if @current_resource.caps == get_caps(@current_resource.keyname)
+end
+
+def get_key(keyname)
+  cmd = "ceph auth print_key #{keyname}"
+  Mixlib::ShellOut.new(cmd).run_command.stdout
+end
+
+def get_caps(keyname)
+  cmd = "ceph auth print_caps #{keyname}"
+  Mixlib::ShellOut.new(cmd).run_command.stdout
+end
+
+def auth_set_key(keyname, caps)
+  ruby_block "set key: #{keyname}" do
+    block do
+      set_cmd = "ceph auth get-or-create #{keyname} --#{caps} --name mon. --key='#{node["ceph"]["monitor-secret"]}'"
+      set_cmd = Mixlib::ShellOut.new(set_cmd)
+      cmd = set_cmd.run_command
+      if cmd.stderr.scan(/EINVAL.*but cap.*does not match/)
+        # delete an old key if it exists and is wrong
+        Mixlib::ShellOut.new("ceph auth del #{keyname}").run_command
+        set_cmd.run_command.error!
+      end
+    end
   end
 end
--- resources/client.rb
+++ resources/client.rb
@@ -2,13 +2,14 @@ actions :add
 default_action :add
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :description, :name_attribute => true, :kind_of => String, :default => nil
-attribute :caps, :default => {"mon"=>"allow r", "osd"=>"allow r"}
+attribute :caps, :kind_of => [Hash, String], :default => {"mon"=>"allow r", "osd"=>"allow r"}
 
 # what the key should be called in the ceph cluster
 # defaults to client.#{name}.#{hostname}
-attribute :keyname, :kind_of => String, :default => nil
+attribute :keyname, :kind_of => String
 
 # where the key should be saved
 # defaults to /etc/ceph/ceph.client.#{name}.#{hostname}.keyring
-attribute :filename, :kind_of => String, :default => nil
+attribute :filename, :kind_of => String
+
+attr_accessor :exists
