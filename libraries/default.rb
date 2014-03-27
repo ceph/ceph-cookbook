02dde7b534ba59dc22c44e3ef3a977f2212c9a85
Walter  Huf <walter.huf@corvisa.com>
2014-03-27 12:19:56 -0500

    Boolean functions should end in ?

--- libraries/default.rb
+++ libraries/default.rb
@@ -34,13 +34,13 @@ def find_node_ip_in_network(network, nodeish = nil)
   nodeish['network']['interfaces'].each do |iface, addrs|
     addresses = addrs['addresses'] || []
     addresses.each do |ip, params|
-      return ip_address_to_ceph_address(ip, params) if ip_address_in_network(ip, params, net)
+      return ip_address_to_ceph_address(ip, params) if ip_address_in_network?(ip, params, net)
     end
   end
   nil
 end
 
-def ip_address_in_network(ip, params, net)
+def ip_address_in_network?(ip, params, net)
   ['inet', 'inet6'].include?(params['family']) && net.include?(ip)
 end
 
