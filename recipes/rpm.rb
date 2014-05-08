882d4cb1dd629c0253f00a960b4811b0a34a31b9
Walter  Huf <walter.huf@corvisa.com>
2014-05-08 17:14:40 -0500

    Don't try to install btrfs-tools on rhel5

--- recipes/rpm.rb
+++ recipes/rpm.rb
@@ -24,7 +24,9 @@ end
 package 'parted'    # needed by ceph-disk-prepare to run partprobe
 package 'hdparm'    # used by ceph-disk activate
 package 'xfsprogs'  # needed by ceph-disk-prepare to format as xfs
-package 'btrfs-progs' # needed to format as btrfs, in the future
+if node['platform_family'] == 'rhel' && node['platform_version'].to_f > 6
+  package 'btrfs-progs' # needed to format as btrfs, in the future
+end
 if node['platform_family'] == 'rhel' && node['platform_version'].to_f < 7
   package 'python-argparse'
 end
