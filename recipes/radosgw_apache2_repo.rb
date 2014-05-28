f226731bfb47154254c3c382ee4f9bdbddcf848f
Walter  Huf <walter.huf@corvisa.com>
2014-05-28 15:54:12 -0500

    Enables the Yum repo for the Apache fork

--- recipes/radosgw_apache2_repo.rb
+++ recipes/radosgw_apache2_repo.rb
@@ -15,6 +15,18 @@ if node['ceph']['radosgw']['use_apache_fork'] == true
       components ['main']
       key 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc'
     end
+  elsif (node.platform_family?('fedora') && [18, 19].include?(node['platform_version'].to_i)) ||
+    (node.platform_family?('rhel') && [6].include?(node['platform_version'].to_i))
+    platform_family = node['platform_family']
+    platform_version = node['platform_version'].to_i
+    yum_repository 'ceph-apache2' do
+      baseurl "http://gitbuilder.ceph.com/apache2-rpm-#{node['platform']}#{platform_version}-x86_64-basic/ref/master"
+      gpgkey node['ceph'][platform_family]['dev']['repository_key']
+    end
+    yum_repository 'ceph-modfastcgi' do
+      baseurl "http://gitbuilder.ceph.com/mod_fastcgi-rpm-#{node['platform']}#{platform_version}-x86_64-basic/ref/master"
+      gpgkey node['ceph'][platform_family]['dev']['repository_key']
+    end
   else
     Log.info("Ceph's Apache and Apache FastCGI forks not available for this distribution")
   end
