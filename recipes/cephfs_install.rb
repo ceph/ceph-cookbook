a3470c1628a51ff15e40ab6c979a87fb17d7fde7
Walter Huf <walter.huf@corvisa.com>
2014-09-19 13:57:49 -0500

    Updates the mount.fuse.ceph helper for pre-firefly

--- recipes/cephfs_install.rb
+++ recipes/cephfs_install.rb
@@ -3,3 +3,10 @@ include_recipe 'ceph::_common_install'
 node['ceph']['cephfs']['packages'].each do |pck|
   package pck
 end
+
+# Update the fuse.ceph helper for pre-firefly
+remote_file '/sbin/mount.fuse.ceph' do
+  source 'https://raw.githubusercontent.com/ceph/ceph/master/src/mount.fuse.ceph'
+  only_if { ::File.exist?('/sbin/mount.fuse.ceph') }
+  not_if { ::File.readlines('/sbin/mount.fuse.ceph').grep(/_netdev/).any? }
+end
