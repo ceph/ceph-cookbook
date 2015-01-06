734b07d0cda25defbda373c4678e071c1eb2e8d3
Walter Huf <walter.huf@corvisa.com>
2015-01-06 00:01:19 -0600

    Installs the ceph-fuse package for RPM distros

--- attributes/cephfs.rb
+++ attributes/cephfs.rb
@@ -6,6 +6,8 @@ when 'debian'
   packages = ['ceph-fs-common', 'ceph-fuse']
   packages += debug_packages(packages) if node['ceph']['install_debug']
   default['ceph']['cephfs']['packages'] = packages
+when 'rhel', 'fedora', 'suse'
+  default['ceph']['cephfs']['packages'] = ['ceph-fuse']
 else
   default['ceph']['cephfs']['packages'] = []
 end
