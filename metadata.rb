1bf37847189e1869e1b0aa2ac4f00b5857f5fa8e
Guilhem Lettron <guilhem@lettron.fr>
2015-02-18 17:06:10 +0100

    Update maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'ceph'
-maintainer 'Kyle Bader'
-maintainer_email 'kyle.bader@dreamhost.com'
+maintainer 'Guilhem Lettron'
+maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures the Ceph distributed filesystem'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
