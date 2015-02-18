3fab16979edb3d2e905625be658ce0512c928b26
Guilhem Lettron <guilhem@lettron.fr>
2015-02-18 17:09:38 +0100

    Release 0.8.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures the Ceph distributed filesystem'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.8.0'
 
 depends	'apache2', '>= 1.1.12'
 depends 'apt'
