d33b62f51781a1f16f7c59a424fe6dd21d5b7d9c
Tim Smith <tsmith84@gmail.com>
2015-09-15 15:13:01 -0700

    Add source_url and issue_url to the metadata
    This is new metadata for Chef 12 that are used by Supermarket

--- metadata.rb
+++ metadata.rb
@@ -10,3 +10,6 @@ depends	'apache2', '>= 1.1.12'
 depends 'apt'
 depends 'yum', '>= 3.0'
 depends 'yum-epel'
+
+source_url 'https://github.com/ceph/ceph-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/ceph/ceph-cookbook/issues' if respond_to?(:issues_url)
